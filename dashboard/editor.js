1d6d4d21843269d623e6b009cfe68c3310ea6472
James-Livesey <jamesliveg@gmail.com>
2021-01-25 13:17:51 +0000

    Add basic editor layout

---
+++ dashboard/editor.js
@@ -0,0 +1,37 @@
+var csengine = require("com.subnodal.codeslate.engine");
+
+namespace("com.subnodal.nanoplay.website.editor", function(exports) {
+    var subElements = require("com.subnodal.subelements");
+    var csengine = require("com.subnodal.codeslate.engine");
+
+    var cseInstance = null;
+
+    subElements.ready(function() {
+        cseInstance = new csengine.CodeslateEngine(document.getElementById("codeEditor"), {
+            languageData: {
+                syntax: [
+                    {type: "comment", regex: `(?<=^(?:[\x03\x05].*?\x04)*[^\x03\x04\x05\n]*)(\\/\\/.*?)\\n`, flags: "m"},
+                    {type: "string", regex: `(?<!\\/\\*)"(?![^\\/]*\\*\\/)(?:[^"\\\\\x04]|\\\\.)*?"`, spellcheck: true},
+                    {type: "string", regex: `(?<!\\/\\*)'(?![^\\/]*\\*\\/)(?:[^'\\\\\x04]|\\\\.)*'`, spellcheck: true},
+                    {type: "string", regex: `(?<!\\/\\*)\`(?![^\\/]*\\*\\/)(?:[^\`\\\\\x04]|\\\\.)*\``, spellcheck: true, flags: "s"},
+                    {type: "comment", regex: `\\/\\*.*?\\*\\/`, flags: "s"},
+                    {type: "regex", regex: `(?<!\\/\\*)(?<=[\\(\\[\\n\\!\\?\\:\\|\\=])\\s*\\/(?![^\\/]*\\*\\/)(?:[^\\/\\\\\x04\\n]|\\\\.)*?\\/`},
+                    {type: "definition", regex: `\\b(function|var|const|let|class|async|await|=>)\\b`},
+                    {type: "keyword", regex: `\\b(if|else|for|in|do|while|try|catch|finally|break|continue|return|with|yield|new|typeof|instanceof|delete|import|export)\\b`},
+                    {type: "number", regex: `\\b(?<!\\$)0(x|X)[0-9a-fA-F]+n?\\b(?!\\$)`}, // Hex
+                    {type: "number", regex: `\\b(?<!\\$)0(b|B)[01]+n?\\b(?!\\$)`}, // Bin
+                    {type: "number", regex: `\\b(?<!\\$)0(o|O)?[0-7]+n?\\b(?!\\$)`}, // Oct
+                    {type: "number", regex: `\\b(?<!\\$)(?:(?:\\b[0-9]+(\\.)[0-9]+[eE][+-]?[0-9]+\\b)(?:\\b[0-9]+(\\.)[eE][+-]?[0-9]+\\b)|(?:\\B(\\.)[0-9]+[eE][+-]?[0-9]+\\b)|(?:\\b[0-9]+[eE][+-]?[0-9]+\\b)|(?:\\b[0-9]+(\\.)[0-9]+\\b)|(?:\\b[0-9]+(\\.)\\B)|(?:\\B(\\.)[0-9]+\\b)|(?:\\b[0-9]+n?\\b(?!\\.)))(?!\\$)`},
+                    {type: "atom", regex: `\\b(true|false|null|undefined|NaN|Infinity)\\b`},
+                    {type: "operator", regex: `(\\+\\+|--|\\+|-|\\*|\\/|%|===|==|!==|!=|\\+=|-=|\\*=|\\/=|%=|=|<<|>>|<=|>=|<|>|&&|\\|\\||!|&|\\||~|\\^|\\?|:)`}
+                ],
+                indentOpenChars: ["(", "{", "["],
+                indentCloseChars: [")", "}", "]"]
+            }
+        });
+
+        cseInstance.onReady(function() {
+            cseInstance.code = `function start() {\n    \n}\n\nfunction loop() {\n    \n}`;
+        });
+    });
+});
